00be0bc54c90056c32dff6fde0e325ea0a5644ed
tanishpate <tanishqpatel2022@gmail.com>
2022-07-21 11:37:44 +0530

    ADD EMPTY PROJECT

---
+++ GAME/Assets/project/script/ScoreArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour {
+
+	public GameObject effectObject;
+
+	// Use this for initialization
+	void Start () {
+		effectObject.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider otherCollider) {
+		if(otherCollider.GetComponent<ball> () != null)
+        {
+			effectObject.SetActive(true);
+        }
+		
+	}
+}
---
+++ GAME/Assets/project/script/ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+	public GameObject trailObject;
+
+	// Use this for initialization
+	void Start () {
+		trailObject.SetActive(false);
+		
+	}
+	
+	public void ActivateTrail()
+    {
+		trailObject.SetActive(true);
+    }
+}
